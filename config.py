6a91eae8602c3a18f4a102bd697b64386f481857
MeKLiN2 <48744549+MeKLiN2@users.noreply.github.com>
2022-04-03 20:57:11 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,52 @@
+ACCOUNT = 'raise'
+PASSWORD = 'jim123'
+BOTNICK = 'Unnameduser_bot'
+# Define rooms the bot will manage in a list format : ['room1','room2','room3']
+ROOMS = ['irony']
+B_ANNOUNCEMENT = 'yes'
+
+
+FALLBACK_RTC_VERSION = '2.0.22-4'
+
+CHAT_LOGGING = True
+
+DEBUG_MODE = False
+
+DEBUG_TO_FILE = False
+
+DEBUG_LEVEL = 30
+
+CONSOLE_COLORS = False
+
+USE_24HOUR = True
+
+DEBUG_FILE_NAME = 'pinylib_debug.log'
+
+CONFIG_PATH = 'rooms/'
+B_PREFIX = '!'
+
+
+B_MAX_MATCH_BANS = 1
+
+B_PUBLIC_CMD = True
+
+B_GREET = False
+
+B_ALLOW_BROADCASTS = True
+
+B_ALLOW_GUESTS = True
+
+B_ALLOW_LURKERS = True
+
+B_ALLOW_GUESTS_NICKS = True
+
+B_USE_KICK_AS_AUTOBAN = False
+
+B_FORGIVE_AUTO_BANS = False
+
+B_VERBOSE = True
+
+B_VIP = False
+
+B_SPAMP = False
+
